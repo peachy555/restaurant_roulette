a7592da2a2f2bd1d52b1f255e9a4e622a0aa10f4
peachy555 <peach.tuntiwong@gmail.com>
2023-06-06 13:48:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,109 @@
-// Assuming you have a list of restaurants and tags
-var restaurants = [
-  { name: "Restaurant 1", tags: ["tag1", "tag2"] },
-  { name: "Restaurant 2", tags: ["tag2", "tag3"] },
-  // Add more restaurants with tags
-];
+// Array to store the list of restaurants
+var restaurants = [];
 
-// Function to populate the tag dropdown on the main page
-function populateTagDropdown() {
-  var tagDropdown = document.getElementById("tagDropdown");
-  tagDropdown.innerHTML = "<option value='all'>All</option>"; // Option to select all restaurants
-  var allTags = [];
+// Array to store the list of tags
+var tags = [];
 
-  restaurants.forEach(function (restaurant) {
-    restaurant.tags.forEach(function (tag) {
-      if (!allTags.includes(tag)) {
-        allTags.push(tag);
-        var option = document.createElement("option");
-        option.value = tag;
-        option.text = tag;
-        tagDropdown.appendChild(option);
-      }
-    });
-  });
-}
-
-// Function to start the roulette and select a restaurant
+// Function to start the roulette
 function startRoulette() {
-  var tagDropdown = document.getElementById("tagDropdown");
-  var selectedTag = tagDropdown.value;
+  // Get the selected tag from the dropdown
+  var selectedTag = document.getElementById("tagDropdown").value;
 
-  // Filter restaurants based on the selected tag
+  // Filter the restaurants based on the selected tag
   var filteredRestaurants = restaurants.filter(function (restaurant) {
-    return selectedTag === "all" || restaurant.tags.includes(selectedTag);
+    return restaurant.tags.includes(selectedTag);
   });
 
   // Randomly select a restaurant from the filtered list
-  var selectedRestaurant =
-    filteredRestaurants[Math.floor(Math.random() * filteredRestaurants.length)];
+  var randomIndex = Math.floor(Math.random() * filteredRestaurants.length);
+  var selectedRestaurant = filteredRestaurants[randomIndex];
 
-  // Display the selected restaurant name
-  alert("Selected Restaurant: " + selectedRestaurant.name);
+  // Display the selected restaurant
+  document.getElementById("restaurant").textContent = selectedRestaurant.name;
 }
 
-// Call the populateTagDropdown function when the page loads
-window.onload = populateTagDropdown;
+// Function to add a new restaurant
+function addNewRestaurant() {
+  var newRestaurantName = document.getElementById("newRestaurantInput").value;
+  restaurants.push({ name: newRestaurantName, tags: [] });
+  populateDropdowns();
+  populateRestaurantTable();
+  document.getElementById("newRestaurantInput").value = "";
+}
+
+// Function to add a new tag
+function addNewTag() {
+  var newTagName = document.getElementById("newTagInput").value;
+  tags.push(newTagName);
+  populateDropdowns();
+  populateTagTable();
+  document.getElementById("newTagInput").value = "";
+}
+
+// Function to tag an existing restaurant with an existing tag
+function tagRestaurant() {
+  var selectedRestaurantIndex = document.getElementById("existingRestaurantDropdown").selectedIndex;
+  var selectedTagIndex = document.getElementById("existingTagDropdown").selectedIndex;
+
+  if (selectedRestaurantIndex >= 0 && selectedTagIndex >= 0) {
+    var selectedRestaurant = restaurants[selectedRestaurantIndex];
+    var selectedTag = tags[selectedTagIndex];
+    selectedRestaurant.tags.push(selectedTag);
+    populateRestaurantTable();
+  }
+}
+
+// Function to delete a restaurant
+function deleteRestaurant() {
+  var selectedRestaurantIndex = document.getElementById("deleteRestaurantDropdown").selectedIndex;
+
+  if (selectedRestaurantIndex >= 0) {
+    restaurants.splice(selectedRestaurantIndex, 1);
+    populateDropdowns();
+    populateRestaurantTable();
+  }
+}
+
+// Function to populate the dropdowns and tables
+function populateDropdowns() {
+  var tagDropdown = document.getElementById("tagDropdown");
+  var existingRestaurantDropdown = document.getElementById("existingRestaurantDropdown");
+  var existingTagDropdown = document.getElementById("existingTagDropdown");
+  var deleteRestaurantDropdown = document.getElementById("deleteRestaurantDropdown");
+
+  // Clear dropdowns
+  tagDropdown.innerHTML = "";
+  existingRestaurantDropdown.innerHTML = "";
+  existingTagDropdown.innerHTML = "";
+  deleteRestaurantDropdown.innerHTML = "";
+
+  // Populate dropdowns
+  tags.forEach(function (tag) {
+    var option = document.createElement("option");
+    option.text = tag;
+    tagDropdown.add(option);
+
+    option = document.createElement("option");
+    option.text = tag;
+    existingTagDropdown.add(option);
+  });
+
+  restaurants.forEach(function (restaurant) {
+    var option = document.createElement("option");
+    option.text = restaurant.name;
+    existingRestaurantDropdown.add(option);
+
+    option = document.createElement("option");
+    option.text = restaurant.name;
+    deleteRestaurantDropdown.add(option);
+  });
+}
+
+// Function to populate the restaurant table
+function populateRestaurantTable() {
+  var restaurantTableBody = document.getElementById("restaurantTableBody");
+  restaurantTableBody.innerHTML = "";
+
+  restaurants.forEach(function (restaurant) {
+    var row = document.createElement("tr");
+
